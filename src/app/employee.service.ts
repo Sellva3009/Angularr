95a3c7a352eee241856002bd1e2649786aee38b4
Selva <sellva3009@gmail.com>
2023-09-10 15:02:57 +0530

    Service Error Handling

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { ErrorHandler, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { IEmployee } from './employee';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  private _url: string = '../assets/data/employees1.json';
 
-  private _url: string = "../assets/data/employees.json";
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getEmployee(): Observable<IEmployee> {
     // return [
@@ -19,6 +19,18 @@ export class EmployeeService {
     //   { id: 3, name: 'Rohit', age: 34 },
     //   { id: 4, name: 'Dhawan', age: 36 },
     // ];
-    return this.http.get<IEmployee>(this._url);
+    return this.http
+      .get<IEmployee>(this._url)
+      .pipe(
+      catchError((error: any) => {
+        console.error('Error:', error);
+        return throwError(error);
+      })
+      );
   }
+
+  // private errorHandler(error: any): Observable<never> {
+  //   console.error('Error:', error);
+  //   return throwError(error);
+  // }
 }
