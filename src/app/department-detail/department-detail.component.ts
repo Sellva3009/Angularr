b7047d9b410d5ccd2de72bcdb56137d298c5ad76
Selva <sellva3009@gmail.com>
2023-09-10 19:12:24 +0530

    paramMap

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,27 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router, ParamMap } from '@angular/router'
 
 @Component({
   selector: 'app-department-detail',
   template: `
     <h3>You have selected the department with {{ departmentId }}</h3>
+    <a (click)="goPrevious()">Previous</a>
+    <a (click)="goNext()">Next</a>
   `,
   styles: [],
 })
 export class DepartmentDetailComponent implements OnInit {
   public departmentId: any;
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-    const idParam = this.route.snapshot.paramMap.get('id');
-    if (idParam !== null) {
-      const id = parseInt(idParam, 10);
-      this.departmentId = id;
-    } else {
-      // Handle the case where 'id' is null.
-      this.departmentId = null;
-    }
-
-    
+    // const idParam = this.route.snapshot.paramMap.get('id');
+    // if (idParam !== null) {
+    //   const id = parseInt(idParam, 10);
+    //   this.departmentId = id;
+    // } else {
+    //   this.departmentId = null;
+    // }
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let idParam = params.get('id');
+      if (idParam !== null) {
+        const id = parseInt(idParam, 10);
+        this.departmentId = id;
+      } else {
+        this.departmentId = null;
+      }
+    });
+  }
+  goPrevious() {
+    let previousId = this.departmentId - 1;
+    this.router.navigate(['/departments', previousId]);
+  }
+  goNext() {
+    let nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
   }
 }
